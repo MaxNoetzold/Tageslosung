8a9c23dab5e4f758dc05eae105ffab79339c1e40
MaxNoetzold <max.noetzold@gmail.com>
2023-12-03 21:10:26 +0100

    add new bundle

--- build/bundle.js
+++ build/bundle.js
@@ -604,7 +604,10 @@
       }
       function getTodayString() {
         const today = /* @__PURE__ */ new Date();
-        return `${today.getDate()}.${today.getMonth() + 1}.${today.getFullYear()}`;
+        const day = String(today.getDate()).padStart(2, "0");
+        const month = String(today.getMonth() + 1).padStart(2, "0");
+        const year = today.getFullYear();
+        return `${day}.${month}.${year}`;
       }
       function getTodaysLosung(rows) {
         const todayString = getTodayString();
